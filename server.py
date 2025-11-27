fca96662f65dd14e163034750e5c96f206369bf3
user <user@MacBook-Pro-user.local>
2025-11-27 18:20:28 +0300

    feat: installed UI dashboard and connected FastAPI static + templates

---
+++ server.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='web/static'), name='static')
+templates = Jinja2Templates(directory='web/templates')
+
+@app.get('/', response_class=HTMLResponse)
+async def ui_root(req: Request):
+    return templates.TemplateResponse('index.html', {"request": req})
+
