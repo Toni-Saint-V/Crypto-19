4c24b3595eb1cdb3042b339948377a95ef73587d
user <user@MacBook-Pro-user.local>
2025-11-27 20:31:21 +0300

    fix: restored web package + ws_live_patch

---
+++ web/ws_live_patch.py
@@ -0,0 +1,16 @@
+import asyncio, time, random
+
+async def ws_live_loop(ws):
+    price = 50000
+    while True:
+        ts = int(time.time())
+        candle = {
+            "time": ts,
+            "open": price,
+            "high": price + 50,
+            "low": price - 50,
+            "close": price + 10
+        }
+        await ws.send_json({"type": "candle", "data": candle})
+        price += random.randint(-30, 30)
+        await asyncio.sleep(1)
